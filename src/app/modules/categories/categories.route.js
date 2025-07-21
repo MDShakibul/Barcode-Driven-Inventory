b4e7dbdf11a145387fe825e514808124b9eafa0b
MDShakibul <shakib6610@gmail.com>
2025-07-21 23:05:01 +0600

    add auth middleware for add categories

--- src/app/modules/categories/categories.route.js
+++ src/app/modules/categories/categories.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { CategoriesController } from './categories.controller.js';
+import auth from '../../middlewares/auth.js';
 
 const router = express.Router();
 
-router.patch('/add-category', CategoriesController.addCategory);
+router.patch('/add-category', auth(), CategoriesController.addCategory);
 router.get('/', CategoriesController.allCategories);
 
 
