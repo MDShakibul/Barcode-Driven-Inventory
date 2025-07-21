e19eb7949eae449763b427ae8d6037413282df9f
MDShakibul <shakib6610@gmail.com>
2025-07-21 23:13:50 +0600

    add auth middleware

--- src/app/modules/products/products.route.js
+++ src/app/modules/products/products.route.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 router.patch('/add-product', ProductsController.addProducts);
 router.get('/product-details/:barcode', ProductsController.productDetail);
-router.patch('/:id', ProductsController.updateProductCategory);
-router.get('/', auth(), ProductsController.allProducts);
+router.patch('/:id', auth(),ProductsController.updateProductCategory);
+router.get('/', ProductsController.allProducts);
 
 
 export const ProductsRoutes = router;
