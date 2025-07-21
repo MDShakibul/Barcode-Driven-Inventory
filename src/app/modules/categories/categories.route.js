6561b04ca21a70c59261fa24d4b9049b69a05bca
MDShakibul <shakib6610@gmail.com>
2025-07-21 23:01:50 +0600

    remove auth from all categories

--- src/app/modules/categories/categories.route.js
+++ src/app/modules/categories/categories.route.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import { CategoriesController } from './categories.controller.js';
-import auth from '../../middlewares/auth.js';
 
 const router = express.Router();
 
 router.patch('/add-category', CategoriesController.addCategory);
-router.get('/', auth(), CategoriesController.allCategories);
+router.get('/', CategoriesController.allCategories);
 
 
 export const CategoriesRoutes = router;
